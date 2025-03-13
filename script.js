4b9121eb38978e01de008bcfbc94d7ca52324d54
DaveBett <davide.betterini@gmail.com>
2025-03-13 12:44:39 +0000

    added a restart button

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#restart");
 
 function makeDivs(numDivs) {
     for (let i = 0; i < numDivs; i++){
-        let cells = document.createElement('div');
-        cells.setAttribute("class", "square");
-        container.appendChild(cells)    
+        let cell = document.createElement('div');
+        cell.setAttribute("class", "square");
+        container.appendChild(cell)    
         }
     }
 makeDivs(256);
 
-
-document.body.addEventListener('mouseover', e => {
-    let passed = e.target.closest('.square');
+document.body.addEventListener("mouseover", e => {
+    let passed = e.target.closest(".square");
     if (!passed) { return; }
-    passed.style.backgroundColor = 'black';
+    passed.style.backgroundColor = "black";
 });
+
+button.addEventListener("click", () => {
+    document.querySelectorAll('.square').forEach(e => e.remove());
+    makeDivs(256);
+});
+
